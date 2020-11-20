64dd876c09d91626da6cfbae55d4ae73e099c8fb
DESKTOP-B4I60VO\heybys <byn0174@gmail.com>
2020-11-20 23:53:30 +0900

    Init product-process

---
+++ product-process/src/main/java/com/msa/productprocess/ProductProcessApplication.java
@@ -0,0 +1,13 @@
+package com.msa.productprocess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductProcessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductProcessApplication.class, args);
+	}
+
+}
---
+++ product-process/src/test/java/com/msa/productprocess/ProductProcessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msa.productprocess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductProcessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
